06e71f3091e590293c3274a961a85daaaadc61b5
Mr1QAZ2WSX <studentchess92@gmail.com>
2026-07-30 22:42:57 +0330

    my first code

---
+++ 1.py
@@ -0,0 +1,13 @@
+sen = 100
+if sen > 50 :
+    print ("oh oh pir shodi")
+    print ("forsat zendegi tamam shod haa")
+if sen < 50 :
+    print ("oh khodaro shokr javooni")
+    print ("zendegi to edamh bedh")    
+    
+        
+    
+    
+    
+
